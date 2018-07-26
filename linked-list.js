b835aa8fec7e9339ed5c1e9ba30f0b27da6ef9b3
BuddhaL1T3 <jmriggs75@gmail.com>
2018-07-26 11:36:54 -0400

    set up node class and linkedList class

---
+++ linked-list.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const node = require('./node');
+const _Node = new node();
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) {
+    let currNode = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return currNode;
+  }
+
+  remove(item) {
+    if (!this.head) {
+      return null;
+    }
+
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let currNode = this.head;
+    let prevNode = this.head;
+
+    while (currNode !== null && currNode.value !== item) {
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+
+    if (currNode === null) {
+      console.log('Item Not Found');
+      return;
+    }
+
+    prevNode.next = currNode.next;
+  }
+}
+
+module.exports = LinkedList;
---
+++ node.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+    //prev
+  }
+}
+
+module.exports = _Node;
